2f5028d2ac212a1e5a8e1bf71846e5064897b94d
AngelMercado <myjava@outlook.es>
2017-08-31 22:15:32 -0500

    Refactor: change header configuration
    config header into app_dev.php and app.php, permit access from any origin,
    accept get,post,options,put,delete.
    
    If the client make a request with a file in any param , this configuration
    die the execution and proccess it like POST.

--- web/app.php
+++ web/app.php
@@ -2,6 +2,14 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Request-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER["REQUEST_METHOD"];
+if ($method == "OPTIONS"){
+    die();
+}
 /**
  * @var Composer\Autoload\ClassLoader
  */
